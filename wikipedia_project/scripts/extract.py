c542341e5b99c098f7f059bfb01626660ec226d9
Feyisayo Ajiboye <solapeajiboye@gmail.com>
2025-12-28 18:35:26 +0100

    initial commit

--- wikipedia_project/scripts/extract.py
+++ wikipedia_project/scripts/extract.py
@@ -79,8 +79,8 @@ def extract_pageviews_by_date(
 
 
 if __name__ == "__main__":
-    # December 17, 2025 at 4 PM (UTC)
-    test_date = datetime(2025, 12, 17, 16, tz="UTC")
+    # December 1, 2025 at 4 PM (UTC)
+    test_date = datetime(2025, 12, 1, 12, tz="UTC")
 
     try:
         output_file = extract_pageviews_by_date(
